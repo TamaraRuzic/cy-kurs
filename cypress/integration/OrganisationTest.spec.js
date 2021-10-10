5eb86368269bbdf710310cc8dfcec3374e12704e
Tamara Ruzic <tamara.ruzic@vivifyideas.com>
2021-10-10 20:45:56 +0200

    Organization tests

---
+++ cypress/integration/OrganisationTest.spec.js
@@ -0,0 +1,95 @@
+import organisation from "../fixtures/organisation.json";
+import sidebar from "../fixtures/sidebar.json";
+import navigation from "../fixtures/navigation.json";
+import loginPage from "../fixtures/login.json";
+import data from "../fixtures/data.json"
+
+describe('Organization test block', () => {
+    it('login', () => {
+        cy.visit('/');
+        cy.get(loginPage.loginEmail).clear().type(data.user.email);
+        cy.get(loginPage.loginPass).clear().type(data.user.password);
+        cy.get(loginPage.submitBtn).click();
+    })
+
+    it('create organization', () => {
+        //cy.visit('/my-organizations');
+        cy.get(organisation.addingOrganisation.addNewOrganisation, {timeout : 5000}).click();
+        cy.get(organisation.addingOrganisation.organisationName).type(data.newOrganisation.organisationName);
+        cy.get(organisation.addingOrganisation.nextButton, {timeout : 3000}).click();
+        cy.get(organisation.addingOrganisation.nextButton, {timeout: 3000}).click();
+        cy.get(organisation.addingOrganisation.okBtn).click();
+
+    });
+
+    it('Edit organization name failed', () => {
+        cy.get(organisation.organisationSidebar.settings).click();
+        cy.get(organisation.organisationInfo.organisationName).clear();
+        cy.get(organisation.organisationInfo.updateNameBtn).click();
+    });
+
+    it('Edit organization name', () => {
+        cy.get(organisation.organisationSidebar.settings).click();
+        cy.get(organisation.organisationInfo.organisationName).type(data.newOrganisation.newOrganisationName);
+        cy.get(organisation.organisationInfo.updateNameBtn).click();
+    });
+
+    it('select workdays and set calendar start day', () => {
+        cy.get(organisation.organisationInfo.checkboxFri);
+        cy.get(organisation.organisationInfo.checkboxMonday);
+        cy.get(organisation.organisationInfo.startDaydropdown).click();
+        cy.get(organisation.organisationInfo.dropdownItemMon);
+    });
+
+    it('vacation days per year failed', () => {
+        cy.get(organisation.organisationInfo.vacationDays).clear();
+        cy.get(organisation.organisationInfo.updateVacation).click();
+    });
+
+    it('vacation days per year', () => {
+        cy.get(organisation.organisationInfo.vacationDays).type(data.newOrganisation.daysPerYear);
+        cy.get(organisation.organisationInfo.updateVacation).click();
+    });
+
+    it('working months required for vacation failed', () => {
+        cy.get(organisation.organisationInfo.workingMonths).clear();
+        cy.get(organisation.organisationInfo.additionalVacation).clear();
+        cy.get(organisation.organisationInfo.updateVacation).click();
+    });
+
+    it('working months, additional vacation days granted and unused vacation', () => {
+        cy.get(organisation.organisationInfo.workingMonths).clear().type(data.newOrganisation.monthsRequiredForVacation);
+        cy.get(organisation.organisationInfo.additionalVacation).clear().type(data.newOrganisation.additionalVacationDays);
+        cy.get(organisation.organisationInfo.periodForUnusedVacation).click();
+        cy.get(organisation.organisationInfo.middleOfNextYear).click();
+        cy.get(organisation.organisationInfo.updateVacation).click();
+    });
+
+    it('change ownership', () => {
+        cy.get(organisation.organisationInfo.ownershipDropdown).click();
+        cy.get(organisation.organisationInfo.selectOwner).click();
+    });
+
+    it('cancel delete organisation', () => {
+        cy.get(organisation.organisationInfo.deleteOrganisation).click();
+        cy.get(organisation.organisationInfo.enterPasswordToDelete).type(data.user.password);
+        cy.get(organisation.organisationInfo.noDelete).click();
+    })
+
+    it('close delete modal', () => {
+        cy.get(organisation.organisationInfo.deleteOrganisation).click();
+        cy.get(organisation.organisationInfo.closeDeleteModal).click();
+    })
+
+    it('delete organisation failed', () => {
+        cy.get(organisation.organisationInfo.deleteOrganisation).click();
+        cy.get(organisation.organisationInfo.enterPasswordToDelete).type(data.invalidUser.invalidPassword);
+        cy.get(organisation.organisationInfo.yesDelete).click();
+    })
+
+    it('delete organisation', () => {
+        cy.get(organisation.organisationInfo.deleteOrganisation).click();
+        cy.get(organisation.organisationInfo.enterPasswordToDelete).clear().type(data.user.password);
+        cy.get(organisation.organisationInfo.yesDelete).click();
+    })
+})
--- cypress/integration/registerTest.spec.js
+++ cypress/integration/registerTest.spec.js
@@ -4,8 +4,10 @@ import data from "../fixtures/data.json";
 import sidebar from "../fixtures/sidebar.json";
 
 describe('register test block', () => {
-    it('visit register page', () => {
-        cy.visit('/sign-up?type=yearly&plan=1&event=page-card');
+    it('visit register page and select pricing plan', () => {
+        cy.visit('https://cypress-api.vivifyscrum-stage.com/pricing');
+        cy.get('.vsp-c-pricing-plan-list--annual > :nth-child(1)').scrollIntoView();
+        cy.get(registerPage.starterPack).scrollIntoView().click({force:true});
     });
 
     it('register existing user', () => {
@@ -16,6 +18,7 @@ describe('register test block', () => {
      });
 
     it('register new user with no email', () => {
+        cy.get(registerPage.registerEmail).clear();
         cy.get(registerPage.registerPass).clear().type(data.newUser.registerPass);
         cy.get(registerPage.numberOfUsers).clear().type("10");
         cy.get(registerPage.finishRegistration).click();
@@ -23,7 +26,8 @@ describe('register test block', () => {
     })
 
     it('register new user with wrong email', () => {
-        cy.get(registerPage.registerPass).clear().type(data.invalidUser.invalidEmail);
+        cy.get(registerPage.registerEmail).clear().type(data.invalidUser.invalidEmail);
+        cy.get(registerPage.registerPass).clear().type(data.newUser.registerPass);
         cy.get(registerPage.numberOfUsers).clear().type("10");
         cy.get(registerPage.finishRegistration).click();
 
@@ -33,15 +37,17 @@ describe('register test block', () => {
         cy.get(registerPage.registerEmail).clear().type(data.newUser.registerEmail);
         cy.get(registerPage.registerPass).clear();
         cy.get(registerPage.numberOfUsers).clear().type("10");
+        cy.get(registerPage.pricePlanMonth).click();
         cy.get(registerPage.finishRegistration).click();
 
     })
 
-    //it('register new user successfuly', () => {
-    //    cy.get(registerPage.registerEmail).clear().type(data.newUser.registerEmail);
-    //    cy.get(registerPage.registerPass).clear().type(data.newUser.registerPass);
-    //    cy.get(registerPage.numberOfUsers).clear().type("10");
-    //    cy.get(registerPage.finishRegistration).click();
+//     it('register new user successfuly', () => {
+//        cy.get(registerPage.registerEmail).clear().type(data.newUser.registerEmail);
+//        cy.get(registerPage.registerPass).clear().type(data.newUser.registerPass);
+//        cy.get(registerPage.numberOfUsers).clear().type("10");
+//        cy.get(registerPage.pricePlanMonth).click();
+//        cy.get(registerPage.finishRegistration).click();
 
-   // })
+//    })
 })
