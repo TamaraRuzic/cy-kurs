f73bf9d1de51f861456374260c215fdd93e30798
Tamara Ruzic <tamara.ruzic@vivifyideas.com>
2021-11-02 17:23:01 +0100

    OrganizationTestSpec refactor

--- cypress/integration/OrganisationTest.spec.js
+++ cypress/integration/OrganisationTest.spec.js
@@ -10,117 +10,53 @@ describe('Organization test block', () => {
         loginModule.login({});
         organizationModule.createOrganization({});
         sidebarModule.organizationSettings.should('be.visible').click();
-        // cy.get(organisation.organisationSidebar.settings).click();
     });
 
     afterEach('delete organisation', () => {
-        organizationModule.deleteOrganization();
+        organizationModule.deleteOrganization({});
         cy.url().should('include','my-organizations');
     });
 
     it('Edit organization name failed', () => {
-        cy.get(organisation.organisationInfo.organisationName)
-            .clear()
-            .should('have.value', '');
-        cy.get(organisation.organisationInfo.updateNameBtn).click();
-        
+        organizationModule.editOrganizationName({name : ''});
     });
 
-    it('Edit organization name', () => {
-        cy.get(organisation.organisationInfo.organisationName)
-            .clear()
-            .type(data.newOrganisation.newOrganisationName)
-            .should('have.value',data.newOrganisation.newOrganisationName);
-        cy.get(organisation.organisationInfo.updateNameBtn).click();
-        cy.contains('Successfully updated the Organization name')
-            .should('be.visible');
+    it('Edit organization name successfully', () => {
+        organizationModule.editOrganizationName({});
     });
 
-    it('select workdays and set calendar start day', () => {
-        cy.get(organisation.organisationInfo.checkboxFri)
-            .click()
-            .should('not.be.checked');
-        cy.get(organisation.organisationInfo.startDaydropdown).click();
-        cy.get(organisation.organisationInfo.dropdownItemMon)
-            .click()
-            .should('be.visible')
-            .and('have.text','Monday');
-    });
-
-    it('vacation days per year failed', () => {
-        cy.get(organisation.organisationInfo.vacationDays)
-            .clear()
-            .should('be.empty');
-        cy.get(organisation.organisationInfo.vacationDaysRequired)
-            .should('be.visible');
-        cy.get(organisation.organisationInfo.updateVacation).click();
-    });
-
-    it('vacation days per year', () => {
-        cy.get(organisation.organisationInfo.vacationDays)
-            .clear()
-            .type(data.newOrganisation.daysPerYear)
-            .should('have.value', data.newOrganisation.daysPerYear);
-        cy.get(organisation.organisationInfo.updateVacation).click();
-        cy.contains('Successfully updated vacation days')
-            .should('be.visible');
-    });
-
-    it('working months required for vacation failed', () => {
-        cy.get(organisation.organisationInfo.workingMonths).clear();
-        cy.get(organisation.organisationInfo.workingMonthsRequired)
-            .should('be.visible');
-        cy.get(organisation.organisationInfo.additionalVacation).clear();
-        cy.get(organisation.organisationInfo.additionalDaysRequired)
-            .should('be.visible')
-        cy.get(organisation.organisationInfo.updateVacation).click();
-        
+    it('vacation days, working months and additional days failed', () => {
+        organizationModule.vacationSection({days : '', month : '', additional : ''});        
     });
 
     it('working months, additional vacation days granted and unused vacation', () => {
-        cy.get(organisation.organisationInfo.workingMonths)
-            .clear()
-            .type(data.newOrganisation.monthsRequiredForVacation)
-            .should('have.value', data.newOrganisation.monthsRequiredForVacation);
-        cy.get(organisation.organisationInfo.additionalVacation)
-            .clear()
-            .type(data.newOrganisation.additionalVacationDays)
-            .should('have.value', organisation.organisationInfo.additionalVacation);
-        cy.get(organisation.organisationInfo.periodForUnusedVacation).click();
-        cy.get(organisation.organisationInfo.middleOfNextYear)
-            .click()
-            .should('be.visible');
-        cy.get(organisation.organisationInfo.updateVacation).click();
-    });
-
-    it('change ownership', () => {
-        cy.get(organisation.organisationInfo.ownershipDropdown).click();
-        cy.get(organisation.organisationInfo.selectOwner).click();
-    });
-
-    it('cancel delete organisation', () => {
-        cy.get(organisation.organisationInfo.deleteOrganisation).click();
-        cy.get(organisation.organisationInfo.enterPasswordToDelete)
-            .type(data.user.password)
-            .should('have.value',data.user.password);
-        cy.get(organisation.organisationInfo.noDelete).click();
-        cy.url().should('include','settings');
+        organizationModule.vacationSection({});
     });
 
     it('close delete modal', () => {
-        cy.get(organisation.organisationInfo.deleteOrganisation).click();
-        cy.get(organisation.organisationInfo.closeDeleteModal).click();
+        organizationModule.deleteOrganization({password : ''});
         cy.url().should('include','settings');
-        cy.get(organisation.organisationInfo.deleteOrganisation)
-            .should('be.visible');
+         organizationModule.delOrganization.should('be.visible');
     });
 
-    it.skip('delete organisation failed due to wrong password', () => {
-        organizationModule.deleteOrganization(password = data.invalidUser.invalidPassword);
-        cy.get(organisation.organisationInfo.closeDeleteModal).click();
-        cy.contains('The password is incorrect.', {timeout : 4000})
-            .should('exists')
-            .and('be.visible');
+    it('delete organisation failed due to wrong password', () => {
+        organizationModule.deleteOrganization({password : data.invalidUser.invalidPassword});
     });
+
+     // it('change ownership', () => {
+    //     cy.get(organisation.organisationInfo.ownershipDropdown).click();
+    //     cy.get(organisation.organisationInfo.selectOwner).click();
+    // });
+
+     // it('select workdays and set calendar start day', () => {
+    //     cy.get(organisation.organisationInfo.checkboxFri)
+    //         .click()
+    //         .should('not.be.checked');
+    //     cy.get(organisation.organisationInfo.startDaydropdown).click();
+    //     cy.get(organisation.organisationInfo.dropdownItemMon)
+    //         .click()
+    //         .should('be.visible')
+    //         .and('have.text','Monday');
+    // });
 
 })
--- cypress/models/organisationModule.js
+++ cypress/models/organisationModule.js
@@ -22,22 +22,164 @@ module.exports = {
     get submitDelete() {
         return cy.get("button[class='el-button el-button--success el-button']");
     },
-
-    createOrganization({name = data.newOrganisation.organisationName}) {
-        this.addOrganizaton.should('be.visible').click();
-        this.organizationName.should('be.visible').type(name);
-        this.nextButton.should('be.visible').click();
-        this.nextButton.should('be.visible').click();
-        this.okButton.should('be.visible').click();
+    get closeModalDelete() {
+        return cy.get("button[class='el-button el-button el-button--text el-button--mini el-button--default el-button--mini']");
+    },
+    get nameOfOrganization() {
+        return cy.get("input[placeholder='Enter name...']");
+    },
+    get modayCheckbox() {
+        return cy.get("div[class='vs-u-pull-left-keep-right'] >div:nth-child(1) [type='checkbox']");
+    },
+    get fridayCheckbox() {
+        return cy.get(":nth-child(5) > .vs-c-checkbox-list-item > .vs-c-checkbox-list-item__check-container > .vs-c-checkbox > .vs-c-checkbox-check");
+    },
+    get startWeekDay() {
+        return cy.get("div[class='vs-c-settings-organization'] > div:nth-child(3) [class='vs-c-btn vs-c-btn--link']");
+    },
+    get startWithModay() {
+        return cy.get("ul[class='el-dropdown-menu vs-c-users-stats-dropdown'] > li:nth-child(1)");
+    },
+    get daysVacation() {
+        return cy.get("input[name='vacationDays']");
+    },
+    get numOfWorkingMonths() {
+        return cy.get("input[name='numOfWorkingMonthsForAdditionalDays']");
+    },
+    get additionalVacationDays() {
+        return cy.get("input[name='additionalVacationDays']");
+    },
+    get submitName() {
+        return cy.get(".vs-c-settings-section-form > .el-form > .vs-u-position-relative > .vs-u-text--left > .vs-c-btn");
+    },
+    get errorNameRequired() {
+        return cy.get(".vs-c-settings-section-form > .el-form > .el-form-item > .vs-c-form-item__error-wrapper > .el-form-item__error");
+    },
+    get errorVacationDaysRequired() {
+        return cy.get(".vs-c-config-vacation-days > .el-form > :nth-child(1) > .vs-c-form-item__error-wrapper > .el-form-item__error");
+    },
+    get errorWorkingMonthsRequired() {
+        return cy.get(".vs-c-config-vacation-days > .el-form > :nth-child(2) > .vs-c-form-item__error-wrapper > .el-form-item__error");
+    },
+    get errorAdditionalDaysRequired() {
+        return cy.get(".vs-c-config-vacation-days > .el-form > :nth-child(3) > .vs-c-form-item__error-wrapper > .el-form-item__error");
+    },
+    get middleNextYear() {
+        return cy.get("div[class='el-select-dropdown__wrap el-scrollbar__wrap el-scrollbar__wrap--hidden-default']> ul > li:nth-child(3)");
+    },
+    get expirationVacationPeriod() {
+        return cy.get("input[placeholder='Select acation days expiration date...']");
+    },
+    get submitVacation() {
+        return cy.get("button[class='vs-c-btn vs-c-btn--primary vs-c-btn--spaced vs-u-font-weight-bold vs-c-btn-auth--top-gap']");
     },
 
-    deleteOrganization(password = data.user.password) {
-        cy.intercept("POST", "**/api/v2/organizations/*").as('delete');
-        this.delOrganization.click();
-        this.confirmPasswordToDelete.should('be.visible', {timeout: 4000}).type(password);
-        this.submitDelete.should('be.visible').click();
-        cy.wait('@delete').then((intercept) => {
-            expect(intercept.response.statusCode).to.eq(201);
-        });
+    createOrganization({ name = data.newOrganisation.organisationName }) {
+        this.addOrganizaton
+            .should('be.visible')
+            .click();
+        this.organizationName
+            .should('be.visible')
+            .type(name);
+        this.nextButton
+            .should('be.visible')
+            .click();
+        this.nextButton
+            .should('be.visible')
+            .click();
+        this.okButton
+            .should('be.visible')
+            .click();
+    },
+
+    deleteOrganization({password = data.user.password}) {
+        if (password == '') {
+            this.delOrganization.click();
+            this.closeModalDelete.should('be.visible').click();
+        } else if(password == data.invalidUser.invalidPassword) {
+            this.delOrganization.click();
+            this.confirmPasswordToDelete
+                .should('be.visible', { timeout: 4000 })
+                .type(password);
+            this.submitDelete
+                .should('be.visible')
+                .click();
+            cy.contains('The password is incorrect.', {timeout : 4000})
+                .should('be.visible');
+            this.closeModalDelete.should('be.visible').click();
+        } else {
+            cy.intercept("POST", "**/api/v2/organizations/*").as('delete');
+            this.delOrganization.click();
+            this.confirmPasswordToDelete
+                .should('be.visible', { timeout: 4000 })
+                .type(password);
+            this.submitDelete
+                .should('be.visible')
+                .click();
+            cy.wait('@delete').then((intercept) => {
+                expect(intercept.response.statusCode).to.eq(201);
+            });
+        }
+
+    },
+
+    editOrganizationName({ name = data.newOrganisation.newOrganisationName }) {
+        if (name == "") {
+            this.nameOfOrganization
+                .should('be.visible')
+                .clear();
+            this.submitName
+                .should('be.visible')
+                .click();
+            this.errorNameRequired
+                .scrollIntoView()
+                .should('be.visible');
+        } else {
+            this.nameOfOrganization
+                .should('be.visible')
+                .clear()
+                .type(name);
+            this.submitName
+                .should('be.visible')
+                .click();
+            cy.contains('Successfully updated the Organization name')
+                .should('be.visible');
+        }
+
+    },
+
+    vacationSection({ days = data.newOrganisation.daysPerYear,
+        month = data.newOrganisation.monthsRequiredForVacation,
+        additional = data.newOrganisation.additionalVacationDays }) {
+        if (days == '') {
+            this.daysVacation.scrollIntoView()
+                .should('be.visible')
+                .clear();
+            this.errorVacationDaysRequired.should('be.visible');
+            this.numOfWorkingMonths.should('be.visible')
+                .clear();
+            this.errorWorkingMonthsRequired.should('be.visible');
+            this.additionalVacationDays.should('be.visible')
+                .clear();
+            this.errorAdditionalDaysRequired.should('be.visible');
+        } else {
+            this.daysVacation.scrollIntoView()
+                .should('be.visible')
+                .clear()
+                .type(days);
+            this.numOfWorkingMonths.should('be.visible')
+                .clear()
+                .type(month);
+            this.additionalVacationDays.should('be.visible')
+                .clear()
+                .type(additional);
+            this.expirationVacationPeriod.should('be.visible')
+                .click();
+            this.middleNextYear.should('be.visible')
+                .click();
+            this.submitVacation.should('be.visible')
+                .click();
+        }
+
     }
 }
